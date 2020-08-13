43b7eba5d194fa6adfbc1d15e7534c8266d3d01f
Nancy Jackson <nbjackson23@gmail.com>
2020-08-13 10:58:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRAND = []
 
   def initialize(brand)
     @brand = brand
@@ -11,5 +13,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-
+  def brand=(brand)
+    @brand = brand
+    BRAND << brand 
+  end
 end
